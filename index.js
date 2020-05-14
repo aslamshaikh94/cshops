244d87ff33b5684dde7b7304bff1e0d37f4dc6f8
aslamahmed1494 <aslamahmed1494@gmail.com>
2020-05-14 21:33:03 +0530

    Changes

--- index.js
+++ index.js
@@ -3,52 +3,52 @@ const app = express();
 const port = process.env.PORT || 5000;
 const path = require('path');
 const fs = require('fs')
+const axios = require('axios')
 
-app.get('/', function(request, response) {
-  console.log('Home page visited!');
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Credentials', true);    
+    next();
+});
+
+const replageHead = (req, res, next)=>{
   const filePath = path.resolve(__dirname, './build', 'index.html');
+  
+  let apiUrl = `https://api.cshops.in${req.url}`
+  let fetchData = new Promise((resolve, reject)=>{
+    axios.get(apiUrl).then((res)=>{
+      resolve(res.data)
+    }) 
+  })
 
-  // read in the index.html file
-  fs.readFile(filePath, 'utf8', function (err,data) {
-    if (err) {
-      return console.log(err);
-    }
-    
-    // replace the special strings with server generated strings
-    data = data.replace(/\$OG_TITLE/g, 'Home Page');
-    data = data.replace(/\$OG_DESCRIPTION/g, "Home page description");
-    result = data.replace(/\$OG_IMAGE/g, 'https://i.imgur.com/V7irMl8.png');
-    response.send(result);
-  });
+  fetchData.then((fdata)=>{    
+      const  {product_name, details, thumbnail} = fdata[0]    
+      // read in the index.html file
+      fs.readFile(filePath, 'utf8', function (err,data) {
+        if (err) {
+          return console.log(err);
+        }
+        
+        // replace the special strings with server generated strings
+        data = data.replace(/\$OG_TITLE/g, product_name);
+        data = data.replace(/\$OG_DESCRIPTION/g, details);
+        let result = data.replace(/\$OG_IMAGE/g, thumbnail);
+        res.send(result);
+        next()
+      });
+  })    
+}
+
+
+app.get('/', replageHead, function(request, response) {
+  
 });
 
-app.get('/about', function(request, response) {
-  console.log('About page visited!');
-  const filePath = path.resolve(__dirname, './build', 'index.html')
-  fs.readFile(filePath, 'utf8', function (err,data) {
-    if (err) {
-      return console.log(err);
-    }
-    data = data.replace(/\$OG_TITLE/g, 'About Page');
-    data = data.replace(/\$OG_DESCRIPTION/g, "About page description");
-    result = data.replace(/\$OG_IMAGE/g, 'https://i.imgur.com/V7irMl8.png');
-    response.send(result);
-  });
+app.get('/product/:id', replageHead, function(request, response) {
+  
 });
 
-app.get('/contact', function(request, response) {
-  console.log('Contact page visited!');
-  const filePath = path.resolve(__dirname, './build', 'index.html')
-  fs.readFile(filePath, 'utf8', function (err,data) {
-    if (err) {
-      return console.log(err);
-    }
-    data = data.replace(/\$OG_TITLE/g, 'Contact Page');
-    data = data.replace(/\$OG_DESCRIPTION/g, "Contact page description");
-    result = data.replace(/\$OG_IMAGE/g, 'https://i.imgur.com/V7irMl8.png');
-    response.send(result);
-  });
-});
+
 
 app.use(express.static(path.resolve(__dirname, './build')));
 

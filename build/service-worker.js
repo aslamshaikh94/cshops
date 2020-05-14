26166b8cfc1118d57debbbd4eda6a86b8e249370
aslamahmed1494 <aslamahmed1494@gmail.com>
2020-05-14 21:20:33 +0530

    Changes

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.58f147084971586192c88d2ad96c04b1.js"
+  "/precache-manifest.8cd08c0a5e666c0bb37ca8d270da69ce.js"
 );
 
 self.addEventListener('message', (event) => {

38466340fa6b8f4132706cf441c4bad07b05dd90
aslamahmed1494 <aslamahmed1494@gmail.com>
2020-05-23 02:12:49 +0530

    Changes

--- index.js
+++ index.js
@@ -1,62 +1,38 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 5500;
+const port = process.env.PORT || 5000;
 const path = require('path');
 const fs = require('fs')
 const axios = require('axios')
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header('Access-Control-Allow-Credentials', true);    
-    next();
+app.get('/product/:id', function(request, response) {
+  const filePath = path.resolve(__dirname, './build', 'index.html');
+
+  // read in the index.html file
+  fs.readFile(filePath, 'utf8', function (err,data) {
+      if (err) {
+        return console.log(err);
+      }     
+      let apiUrl = `https://api.cshops.in${request.url}`
+      axios.get(apiUrl).then((resdata)=>{      
+      const  {product_name, details, thumbnail} = resdata.data[0]
+      // replace the special strings with server generated strings
+      data = data.replace(/\$OG_TITLE/g, product_name);
+      data = data.replace(/\$OG_DESCRIPTION/g, "details");
+      data = data.replace(/\$OG_KEYWORDS/g, "product_name");
+      let result = data.replace(/\$OG_IMAGE/g, "thumbnail");
+      response.send(result);
+    }) 
+  });
 });
+
+
+
 
 app.use(express.static(path.resolve(__dirname, './build')));
 
-const replageHead = (req, res, next)=>{
-  const filePath = path.resolve(__dirname, './build', 'index.html');
-  
-  let apiUrl = `https://api.cshops.in${req.url}`
-  let fetchData = new Promise((resolve, reject)=>{
-    axios.get(apiUrl).then((res)=>{
-      resolve(res.data)
-    }) 
-  })
-
-  fetchData.then((fdata)=>{
-      const  {product_name, details, thumbnail} = fdata[0]    
-      // read in the index.html file
-      fs.readFile(filePath, 'utf8', function (err,data) {
-        if (err) {
-          return console.log(err);
-        }
-        
-        // replace the special strings with server generated strings
-        data = data.replace(/\$OG_TITLE/g, product_name);
-        data = data.replace(/\$OG_DESCRIPTION/g, details);
-        data = data.replace(/\$OG_KEYWORDS/g, product_name);
-        let result = data.replace(/\$OG_IMAGE/g, thumbnail);
-        res.send(result);
-        next()
-      });
-  })    
-}
-
-
-app.get('/', replageHead, function(request, response) {
-  
-});
-
-app.get('/product/:id', replageHead, function(request, response) {
-  
-});
-
-
-
-
-
 app.get('*', function(request, response) {
-  const filePath = path.resolve(__dirname, './build', 'index.html');  
+  const filePath = path.resolve(__dirname, './build', 'index.html');
   response.sendFile(filePath);
 });
 
